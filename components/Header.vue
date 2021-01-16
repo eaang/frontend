3c6c7b4cbfc92352db2132fac21b5dfcababd7c2
Evan Ang <alethiologist@gmail.com>
2021-01-16 13:18:10 +0800

    added a product highlight

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,18 @@
 <template>
-  <client-only>
-    <div class="h-16 bg-purple-800 w-full">
-      <div
-        class="container mx-auto px-4 flex items-center h-full text-white relative"
-      >
-        <div class="absolute inset-y-0 left-0 flex items-center pl-4">
-          <div class="h-8 w-8" @click="$nuxt.$emit('openSidebar')">
-            <Menu />
-          </div>
-        </div>
-        <div class="flex-grow flex justify-center">
-          <router-link to="/"
-            ><div class="font-bold text-lg">Gift Ellie</div></router-link
-          >
+  <div class="h-16 bg-purple-800 w-full">
+    <div
+      class="container mx-auto px-4 flex items-center h-full text-white relative"
+    >
+      <div class="absolute inset-y-0 left-0 flex items-center pl-4">
+        <div class="h-8 w-8" @click="$nuxt.$emit('openSidebar')">
+          <Menu />
         </div>
       </div>
+      <div class="flex-grow flex justify-center">
+        <router-link to="/"
+          ><div class="font-bold text-lg">Gift Ellie</div></router-link
+        >
+      </div>
     </div>
-  </client-only>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,66 @@
 <template>
   <div>
     <Categories />
-    <div class="container mx-auto px-4 h-screen">
-      <div>A simple but memorable gift?</div>
-      <div>How about a hot cup of coffee?</div>
-      <div>
-        <img
-          class="h-24 object-contain"
-          src="https://www.starbucks.com.sg/media/Caff%C3%A8Latte_750x750.jpg"
-          alt=""
-        />
+    <div class="container mx-auto px-6 h-screen space-y-6 py-6">
+      <!-- Highlight box -->
+      <div class="w-full">
+        <div class="title w-full">A simple but memorable gift?</div>
+        <div class="flex items-center justify-between h-48 space-x-4">
+          <div class="space-y-6">
+            <div class="font-semibold text-sm">{{ highlight.description }}</div>
+            <div>
+              <NuxtLink
+                :to="{
+                  name: 'gifts-slug',
+                  params: { slug: highlight.gift.id },
+                }"
+              >
+                <Button text="View" classes="primary small rounded-lg"
+              /></NuxtLink>
+            </div>
+          </div>
+          <div class="w-2/5">
+            <img
+              class="w-full object-cover"
+              :src="imagePrefix + highlight.gift.image.url"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import highlightQuery from '~/apollo/queries/highlight/highlight'
+
+export default {
+  data() {
+    return {
+      highlight: [],
+    }
+  },
+  apollo: {
+    highlight: {
+      prefetch: true,
+      query: highlightQuery,
+    },
+  },
+  computed: {
+    imagePrefix() {
+      if (process.env.NODE_ENV !== 'development') {
+        return ''
+      } else {
+        return this.$config.apiUrl
+      }
+    },
+  },
+}
 </script>
+
+<style scoped>
+.title {
+  @apply font-bold text-lg text-center;
+}
+</style>
