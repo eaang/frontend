6ebc0c2c6cb6dd51c279f4be161571cd638c60b5
Evan Ang <alethiologist@gmail.com>
2021-03-30 19:38:26 +0800

    back to static for now

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,7 @@ export default {
   },
   privateRuntimeConfig: {},
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'server',
-
-  mode: 'universal',
+  target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
